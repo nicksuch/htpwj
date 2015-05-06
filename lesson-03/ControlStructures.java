5215114734a692b836b57332d71d46fddbc952bc
Nick Such <nicksuch@gmail.com>
2015-05-06 13:51:37 -0400

    Complete lesson 3

---
+++ lesson-03/ControlStructures.java
@@ -0,0 +1,10 @@
+public class ControlStructures {
+	public static void main (String[] args) {
+		// Control Structures
+		int age = 21;
+		if (age < 20) {
+			System.out.println("You are less than 20 years old.");
+		}
+		System.out.println("OK, bye!");
+	}
+}
