c719616d3684fec012afc440ca3978c6439e182b
Nick Such <nicksuch@gmail.com>
2015-05-06 13:18:20 -0400

    Add first few lessons

---
+++ lesson-02/VariablesExample.java
@@ -0,0 +1,12 @@
+public class VariablesExample {
+	public static void main (String[] args) {
+		String username = "nicksuch";
+		System.out.println(username);
+
+		Integer math = 2 - 2;
+		System.out.println(math);
+
+		Double math2 = 2.5 - 2;
+		System.out.println(math2);
+	}
+}
